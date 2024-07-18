34657728a1adac2f08fa634d77bfcc896a8405c7
sigidibau <adriansantiago33@gail.com>
2024-07-18 11:25:05 -0400

    borrador

--- task-0.c
+++ task-0.c
@@ -9,31 +9,31 @@ int _printf(const char *format, ...)
 	va_start(arg_list, format);
 
 	i = 0;
-	for(int it = 0; format[i] != NULL; i++)
+	for(int i = 0; format[i] != NULL; i++)
 	{
 		if (format[i] ==  '%')
 			i++;
 	}
+	{
 		if (format[i] == 's')
 			i++;
-	{
-		char *s
-			s = va_arg(arg_list, char*);	      
-	       	int c;
-	for(c = 0; s[c] != NULL; c++)
 	}
+		char *s
+		s = va_arg(arg_list, char*);
+	    int c;
+	for(c = 0; s[c] != NULL; c++)
 	{
 	putchar(s[c]);
 		len++;
-		}
-	else
+	}
+		else
 	{
 	putchar(format[it]);
 	len++;
 	}
 
 	va_end(arg_list);
-	return len;	
+	return len;
 }
 
 
