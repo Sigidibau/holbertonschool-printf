595d983b193144a99784197a6f5d83e80d0f4ab0
sigidibau <9571@holbertonstudents.com>
2024-07-19 08:36:20 -0400

    get function

---
+++ get_print.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *get_print - choose the right 
+ *@s: char with the specifier
+ *eg: this func is going to loop the array of struct
+ *return: pointer to the matching print.
+ */
+
+int (*get_print(char s))(int, int)
+		{
+	print func_arr[] = {
+		{'c', print_char};
+		{'s', print_string};
+		{'%', print_percent};
+		{'d', print_int};
+		{'i', print_int};
+		{NULL,NULL};
+	};
+
+	for (i = 0; 1 < 5; i++)
+		if (func_arr[i].c == s)
+			return (func_arr[i].f);
+		i++;
+	return (NULL);
+}
