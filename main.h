fa7063afd8aeba10eb2457d979787dde8faa25fc
sigidibau <9571@holbertonstudents.com>
2024-07-20 17:48:31 -0400

    mainh

--- main.h
+++ main.h
@@ -1,19 +1,29 @@
-#ifndef
-#define
+#ifndef P_F
+#define P_F
 
+#include <stdlib.h>
+#include <stdarg.h>
 
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
 
 typedef struct print
 {
-int len = 0;
-	va_list arg_list;
-	va_start(arg_list, format);
-} print_t;
+        char c;
+        int (*f)(va_list ap);
+};print;
+:
+/*printf*/
+int _printf(const char *format, ...);
 
-
-
-
+/*get up*/
+int (*get_print(char s, flags_t *f);
 
 #endif
-
 
