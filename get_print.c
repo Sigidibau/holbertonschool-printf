3b8153a92cc5a0a54e300567d79b509180681ae5
sigidibau <9571@holbertonstudents.com>
2024-07-22 08:19:27 -0400

    get func

--- get_print.c
+++ get_print.c
@@ -7,20 +7,22 @@
  *return: pointer to the matching print.
  */
 
-int (*get_print(char s))(va_list)
-		{
-	print func_arr[] = {
-		{'c', prt_char};
-		{'s', prt_string};
-		{'%', prt_percent};
-		{'d', prt_dec};
-		{'i', prt_int};
-		{NULL,NULL};
+unsigned int (*get_specifier(const char ))(va_list, unsigned char, int, int, unsigned char)
+{
+	int i;
+	converter_t converters[] = {
+		{'c', convert_c},
+		{'s', convert_string},
+		{'%', convert_percent},
+		{'d', convert_dec},
+		{'i', convert_int},
+		{NULL,NULL},
 	};
 
-	for (i = 0; 1 < 5; i++)
-		if (func_arr[i].c == s)
-			return (func_arr[i].f);
-		i++;
+	for (i = 0; converters[i].func; i++)
+	{
+		if (converters[i].specifier == *specifier)
+			return (converters[i].func);
+	}
 	return (NULL);
 }
